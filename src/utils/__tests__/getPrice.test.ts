9f95209376ada38d6bb8f186e48a0e7e2f5c9495
zenehu-PC <zeronethunter2001@gmail.com>
2023-03-09 21:01:05 +0300

    add: utils tests

--- src/utils/__tests__/getPrice.test.ts
+++ src/utils/__tests__/getPrice.test.ts
@@ -1,8 +1,14 @@
 import { getPrice } from '../getPrice';
+import { PriceSymbol } from '../../types';
 
 describe('test get price function', () => {
-    it('should return value with price symbol', () => {
-        expect(getPrice(100, '₽')).toBe('100 ₽');
-        expect(getPrice(325, '$')).toBe('325 $');
-    });
+    test.each<{ value: number; symbol: PriceSymbol; expected: string }>([
+        { value: 100, symbol: '$', expected: '100 $' },
+        { value: 150, symbol: '₽', expected: '150 ₽' },
+    ])(
+        'should return correct price: $value, with symbol: $symbol',
+        ({ value, symbol, expected }) => {
+            expect(getPrice(value, symbol)).toBe(expected);
+        }
+    );
 });
