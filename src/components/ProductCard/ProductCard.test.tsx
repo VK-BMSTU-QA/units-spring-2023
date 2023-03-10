5f7ac4e4d774538141d5885d2cf805aa49214879
Liza1040 <maxsimova2002eliza@yandex.ru>
2023-03-10 18:18:33 +0300

    Добавлены тесты на компонент и главную страницу

--- src/components/ProductCard/ProductCard.test.tsx
+++ src/components/ProductCard/ProductCard.test.tsx
@@ -1,41 +1,50 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Categories } from './Categories';
+import { ProductCard } from './ProductCard';
+import { Product } from '../../types';
+import { getPrice } from '../../utils';
+jest.mock('../../utils');
 
 afterEach(jest.clearAllMocks);
-describe('Categories test', () => {
-    it('should render correctly', () => {
-        const rendered = render(<Categories selectedCategories={[]} />);
-
+describe('ProductCard test', () => {
+    it.each([
+        [{
+            id: 2, 
+            name: 'Костюм гуся',
+            description: 'Запускаем гуся, работяги',
+            price: 1000,
+            priceSymbol: '₽',
+            category: 'Одежда',
+            imgUrl: '/lamp.png',
+        } as Product],
+        [{
+            id: 2, 
+            name: 'Костюм гуся',
+            description: 'Запускаем гуся, работяги',
+            price: 1000,
+            priceSymbol: '₽',
+            category: 'Одежда',
+        } as Product],
+    ])('should render correctly', (productMock) => {
+        const rendered = render(<ProductCard key={productMock.id} {...productMock} />);
+        
         expect(rendered.asFragment()).toMatchSnapshot();
     });
 
-    it('should add class for selected badge', () => {
-        const rendered = render(<Categories selectedCategories={['Одежда']} />);
+    it('test getPrice called', () => {
+        const productMock: Product = {
+            id: 2, 
+            name: 'Костюм гуся',
+            description: 'Запускаем гуся, работяги',
+            price: 1000,
+            priceSymbol: '₽',
+            category: 'Одежда',
+            imgUrl: '/lamp.png',
+        }
 
-        expect(rendered.getByText('Одежда')).toHaveClass(
-            'categories__badge_selected'
-        );
-        expect(rendered.getByText('Электроника')).not.toHaveClass(
-            'categories__badge_selected'
-        );
-        expect(rendered.getByText('Для дома')).not.toHaveClass(
-            'categories__badge_selected'
-        );
-    });
-
-    it('should call callback when category click', () => {
-        const onCategoryClick = jest.fn();
-        const rendered = render(
-            <Categories
-                selectedCategories={[]}
-                onCategoryClick={onCategoryClick}
-            />
-        );
-
-        expect(onCategoryClick).not.toHaveBeenCalled();
-        fireEvent.click(rendered.getByText('Одежда'));
-        expect(onCategoryClick).toHaveBeenCalled();
+        render(<ProductCard key={productMock.id} {...productMock} />);
+        
+        expect(getPrice).toHaveBeenCalled();
     });
 });
---
+++ src/pages/MainPage/MainPage.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MainPage } from './MainPage';
+jest.mock('../../hooks/useCurrentTime');
+
+afterEach(jest.clearAllMocks);
+describe('MainPage test', () => {
+    it('test render page without currentTime', () => {
+        const rendered = render(<MainPage/>);
+        
+        expect(rendered.asFragment()).toMatchSnapshot();
+    });
+
+    describe.each([
+        'Для дома',
+        'Одежда',
+        'Электроника'
+    ])('test categories', (category) => {
+        it('should render in page', () => {
+            const rendered = render(<MainPage/>);
+            const rendered_category = rendered.getAllByText(category).filter(item => item.classList.contains('categories__badge'))[0];
+            
+            expect(rendered_category).toBeInTheDocument();
+        });
+
+        it('should filter by categories', () => {
+            const rendered = render(<MainPage/>);
+            const rendered_category = rendered.getAllByText(category).filter(item => item.classList.contains('categories__badge'))[0];
+            fireEvent.click(rendered_category);
+
+            expect(rendered.asFragment()).toMatchSnapshot();
+        });
+    });
+
+    it('sort button should render in page', () => {
+        const rendered = render(<MainPage/>);
+        const sort_button = rendered.getAllByRole('button').filter(item => item.classList.contains('sort-button'))[0];
+        
+        expect(sort_button).toBeInTheDocument();
+    });
+
+    it('test sorting', () => {
+        const rendered = render(<MainPage/>);
+        const sort_button = rendered.getAllByRole('button').filter(item => item.classList.contains('sort-button'))[0];
+        
+        fireEvent.click(sort_button);
+        expect(rendered.asFragment()).toMatchSnapshot();
+    });
+});
