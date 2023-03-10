79b9eb9d26265a99d792dbac8c76859835c4b2ea
FackOff25 <kozvladimir@bk.ru>
2023-03-10 15:26:55 +0300

    productCard tests

--- src/components/ProductCard/ProductCard.test.tsx
+++ src/components/ProductCard/ProductCard.test.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ProductCard } from './ProductCard';
-import { Product } from '../../types';
 
+import getPrice from '../../utils/getPrice';
+jest.mock('../../utils/getPrice');
+
+const mockMyFunction = getPrice as jest.Mock;
+    
 afterEach(jest.clearAllMocks);
 
-describe('Card render', () => {
+describe('product card test', () => {
     it('should render', () => {
         const renderedCard = render(
             <ProductCard
@@ -20,5 +24,48 @@ describe('Card render', () => {
         );
 
         expect(renderedCard.asFragment).toMatchSnapshot();
+    });
+
+    it('should call get price function', () => {
+        expect(mockMyFunction).not.toBeCalled();
+        const renderedCard = render(
+            <ProductCard
+                id={0}
+                name='electrProd'
+                description='very original description'
+                price={100}
+                category='Электроника'
+            />
+        );
+        expect(mockMyFunction).toBeCalledTimes(1);
+    });
+
+    it('should not add image', () => {
+        const renderedCard = render(
+            <ProductCard
+                id={0}
+                name='electrProd'
+                description='very original description'
+                price={100}
+                category='Электроника'
+            />
+        );
+        const image = renderedCard.baseElement.querySelector('img')
+        expect(image).toBe(null);
+    });
+
+    it('should not add image', () => {
+        const renderedCard = render(
+            <ProductCard
+                id={0}
+                name='electrProd'
+                description='very original description'
+                price={100}
+                category='Электроника'
+                imgUrl='url'
+            />
+        );
+        const image = renderedCard.baseElement.querySelector('img')
+        expect(image).not.toBe(null);
     });
 });
