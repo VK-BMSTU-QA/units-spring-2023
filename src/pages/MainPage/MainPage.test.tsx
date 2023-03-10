b1ad82a5c7bcfd31bb0555809954525e163055a2
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2023-03-10 20:42:22 +0300

    deleted mainpage snapshot from commit

--- src/pages/MainPage/MainPage.test.tsx
+++ src/pages/MainPage/MainPage.test.tsx
@@ -9,7 +9,7 @@ afterEach(jest.clearAllMocks);
 
 jest
   .useFakeTimers()
-  .setSystemTime(new Date('2020-01-01'));
+  .setSystemTime(new Date('2020-01-01T00:00:00'));
 
 jest.mock('../../utils/updateCategories', () => {
     return {
